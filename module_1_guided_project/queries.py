2ba9b3c2d1888e1a06c2da9fcc26d54692bde407
Walter Perris <walter.perris@outlook.com>
2024-07-25 06:37:46 -0500

    dell laptop push after windows update before starting new module

---
+++ module_1_guided_project/queries.py
@@ -0,0 +1,12 @@
+SELECT_ALL = 'SELECT character_id, name FROM charactercreator_character;'
+
+
+AVG_ITEM_WEIGHT_PER_CHARACTER = '''
+    SELECT cc_char.name, AVG(ai.weight) AS avg_item_weight 
+    FROM charactercreator_character AS cc_char
+    join charactercreator_character_inventory AS cc_inv
+    ON cc_char.character_id = cc_inv.character_id
+    JOIN armory_item AS ai
+    ON ai.item_id = cc_inv.item_id
+    GROUP BY cc_char.character_id
+    '''
